599f5e4ec63be025a45bd519cd4af51d92d5e714
Ersocaut <leonardo.soy.yo@hotmail.com>
2021-09-22 16:53:13 -0500

    Poblar.sql decanaturas y materias sistemas

---
+++ src/main/sql/poblar.sql
@@ -0,0 +1,131 @@
+------------------------------
+---------POBLAR---------------
+------------------------------
+
+-- Decanatura
+
+insert into decanatura (nombre)
+values ('Departamento de matemáticas');
+insert into decanatura (nombre)
+values ('Departamento de humanidades');
+insert into decanatura (nombre)
+values ('Departamento de ciencias');
+insert into decanatura (nombre)
+values ('Ingeniería de sistemas');
+insert into decanatura (nombre)
+values ('Ingeniería civil');
+insert into decanatura (nombre)
+values ('Ingeniería eléctrica');
+insert into decanatura (nombre)
+values ('Ingeniería industrial');
+insert into decanatura (nombre)
+values ('Ingeniería electrónica');
+insert into decanatura (nombre)
+values ('Ingeniería biomédica');
+insert into decanatura (nombre)
+values ('Ingeniería ambiental');
+insert into decanatura (nombre)
+values ('Ingeniería estadística');
+insert into decanatura (nombre)
+values ('Ingeniería de mecánica');
+insert into decanatura (nombre)
+values ('Matemáticas');
+insert into decanatura (nombre)
+values ('Economía');
+insert into decanatura (nombre)
+values ('Administración de empresas');
+
+-- Materias
+-- Sistemas
+-- 1
+insert into materia (sigla, nombre, creditos)
+values('PREM', 'Precálculo', 4);
+insert into materia (sigla, nombre, creditos)
+values('AGEO', 'Análisis geométrico', 4);
+insert into materia (sigla, nombre, creditos)
+values('FUME', 'Fundamentos de mecánica', 3);
+insert into materia (sigla, nombre, creditos)
+values('INSI', 'Introducción a la ingeniería de sistemas', 1);
+insert into materia (sigla, nombre, creditos)
+values('FCO1', 'Fundamentos de la comunicación 1', 2);
+-- 2
+insert into materia (sigla, nombre, creditos)
+values('CALD', 'Cálculo diferencial', 4);
+insert into materia (sigla, nombre, creditos)
+values('ALLI', 'Álgebra lineal', 3);
+insert into materia (sigla, nombre, creditos)
+values('MMIN', 'Modelos matemáticos para informática', 3);
+insert into materia (sigla, nombre, creditos)
+values('FIME', 'Física neánica', 4);
+insert into materia (sigla, nombre, creditos)
+values('HGCL', 'Historia y geografía de Colombia', 2);
+insert into materia (sigla, nombre, creditos)
+values('FCO2', 'Fundamentos de la comunicación 2', 2);
+-- 3 
+insert into materia (sigla, nombre, creditos)
+values('CALI', 'Cálculo integral', 4);
+insert into materia (sigla, nombre, creditos)
+values('AYPR', 'Algoritmos y programación', 3);
+insert into materia (sigla, nombre, creditos)
+values('EGR1', 'Expresión gráfica 1', 3);
+insert into materia (sigla, nombre, creditos)
+values('LCAT', 'Lógica calculatoria', 4);
+insert into materia (sigla, nombre, creditos)
+values('FIEM', 'Física del electromagnetismo', 4);
+-- 4
+insert into materia (sigla, nombre, creditos)
+values('CALV', 'Cálculo vectorial', 4);
+insert into materia (sigla, nombre, creditos)
+values('AYED', 'Algoritmos y estructuras de datos', 4);
+insert into materia (sigla, nombre, creditos)
+values('MATD', 'Matemáticas discretas', 4);
+insert into materia (sigla, nombre, creditos)
+values('CNYT', 'Ciencias naturales y tecnología', 3);
+insert into materia (sigla, nombre, creditos)
+values('CIPP', 'Colombia políticas y paz', 2);
+-- 5
+insert into materia (sigla, nombre, creditos)
+values('ECDI', 'Ecuaciones diferenciales', 3);
+insert into materia (sigla, nombre, creditos)
+values('MBDA', 'Modelos y bases de datos', 4);
+insert into materia (sigla, nombre, creditos)
+values('TPRO', 'Teoría de la programación', 3);
+insert into materia (sigla, nombre, creditos)
+values('POOB', 'Programación orientada a objetos', 4);
+-- 6
+insert into materia (sigla, nombre, creditos)
+values('PRYE', 'Probabilidad y estadística', 4);
+insert into materia (sigla, nombre, creditos)
+values('ACSO', 'Arquitectura computacional', 4);
+insert into materia (sigla, nombre, creditos)
+values('TSOR', 'Teoría de sistemas y organizaciones', 3);
+insert into materia (sigla, nombre, creditos)
+values('FUEC', 'Fundamentos económicos', 2);
+-- 7
+insert into materia (sigla, nombre, creditos)
+values('RECO', 'Redes de computadores', 4);
+insert into materia (sigla, nombre, creditos)
+values('CVDS', 'Ciclos de vida de desarrollo de software', 4);
+insert into materia (sigla, nombre, creditos)
+values('AUPN', 'Automatización de procesos de negocio', 4);
+-- 8
+insert into materia (sigla, nombre, creditos)
+values('SPTI', 'Seguridad y privacidad de TI', 4);
+insert into materia (sigla, nombre, creditos)
+values('ARSW', 'Arquitecturas de software', 4);
+insert into materia (sigla, nombre, creditos)
+values('FCFI', 'Fundamentos contables y financieros', 3);
+-- 9
+insert into materia (sigla, nombre, creditos)
+values('SIML', 'Seminario de inserción laboral', 1);
+insert into materia (sigla, nombre, creditos)
+values('IETI', 'Innovación y emprendimiento', 4);
+insert into materia (sigla, nombre, creditos)
+values('AREP', 'Arquitecturas empresariales', 4);
+insert into materia (sigla, nombre, creditos)
+values('FDGP', 'Fundamentos de proyectos', 3);
+-- 10
+insert into materia (sigla, nombre, creditos)
+values('OGR1', 'Opción de grado 1', 3);
+insert into materia (sigla, nombre, creditos)
+values('OGR2', 'Opción de grado 2', 3);
