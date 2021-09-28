a1090b6bb4c36ed8ebd9ef8b86ba4624480221ed
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-09-28 15:39:11 -0500

    Exceptions

--- src/main/java/edu/eci/arsw/app/ucord/persistence/UcordPersistenceException.java
+++ src/main/java/edu/eci/arsw/app/ucord/persistence/UcordPersistenceException.java
@@ -1,5 +1,11 @@
 package edu.eci.arsw.app.ucord.persistence;
 
 public class UcordPersistenceException extends Exception {
-    
+    public UcordPersistenceException(String message) {
+        super(message);
+    }
+
+    public UcordPersistenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
