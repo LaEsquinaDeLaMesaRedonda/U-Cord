727e877288f843283d6145041bc80c0ffe544c93
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-09-28 14:42:54 -0500

    Startinfg controllers

---
+++ src/main/java/edu/eci/arsw/app/ucord/controllers/UserAPIController.java
@@ -0,0 +1,15 @@
+package edu.eci.arsw.app.ucord.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ana Gabriela Silva
+ * @author Leonardo Galeano
+ * @author Iván Camilo Rincón Saavedra
+ * @version 9/28/2021
+ */
+@RestController
+@RequestMapping(value = "/U-cord")
+public class UserAPIController {
+}
