d4aca45ee5346270138640b11cf6b19aeeb9a59f
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-10-03 18:27:56 -0500

    Spring JPA Dependencies

---
+++ src/main/java/edu/eci/arsw/app/ucordapi/UCordAPIApplication.java
@@ -0,0 +1,9 @@
+package edu.eci.arsw.app.ucordapi;/**
+  *@author Ana Gabriela Silva
+  *@author Juan Andrés Pico
+  *@author Leonardo Galeano
+  *@author Iván Camilo Rincón Saavedra
+  *@version 10/3/2021
+  *
+*/public class UCordAPIApplication {
+}
