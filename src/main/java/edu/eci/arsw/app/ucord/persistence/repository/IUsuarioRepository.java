5873637e4d11a0fd594ba667a1a4b9ec270b1caa
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2021-10-06 12:49:58 -0500

    Heroku

---
+++ src/main/java/edu/eci/arsw/app/ucord/persistence/repository/IUsuarioRepository.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.app.ucord.repository;
+
+import edu.eci.arsw.app.ucord.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Ana Gabriela Silva
+ * @author Leonardo Galeano
+ * @author Iván Camilo Rincón Saavedra
+ * @version 10/3/2021
+ */
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
